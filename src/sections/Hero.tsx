57e03d83b60a8340b08c6781b746ac3d4a1824c4
Mridul Roy <tandonroy840@gmail.com>
2026-09-17 23:47:54 +0600

    Update resume download link in Hero component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -87,7 +87,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 p-6 h-12 rounded-xl">
             <span>✋</span>
-            <a href="https://drive.google.com/file/d/1at66EP4KR0RdXmDXsLuSFyQkYju5u_5X/view?usp=sharing" className="font-semibold">
+            <a href="https://drive.google.com/file/d/18gCYv_8zeS5ZyrU6R_uQoaRYONUokyIZ/view?usp=sharing" className="font-semibold">
               Download Resume
             </a>
           </button>
